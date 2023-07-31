86934400931d48ac7c5cff08dce8fdd60aa4f17e
vamastah <szymorawski@gmail.com>
2023-07-31 12:17:22 +0200

    add icon, update gitignore, update makefiles

--- minislug0/common.mk
+++ minislug0/common.mk
@@ -9,10 +9,11 @@ PKGCONF ?= $(SYSROOT)/../../bin/pkg-config
 
 OBJECTS = main.o anims.o animspr.o blkanim.o blkbkg.o boss.o dust.o fire.o font.o frame.o game.o gif.o interface.o loader.o menu.o monsters00.o monsters10.o monsters20.o monsters30.o monsters40.o monsters50.o mst.o preca.o psd.o scroll.o sfx.o sprites.o sprcache.o sprrz.o transit2d.o
 
-CFLAGS = -O3 -Wall -s `$(PKGCONF) --cflags sdl` -L. -fno-strict-aliasing -DNDEBUG
+CFLAGS = -O3 -Wall -s `$(PKGCONF) --cflags sdl` -fno-strict-aliasing -DNDEBUG
 LIBS = `$(PKGCONF) --libs sdl` -s ymlib/libymlib.a
 
 all: $(TARGET)
+.PHONY: all
 
 $(TARGET): $(OBJECTS) ymlib/libymlib.a
 	$(CXX) $(CFLAGS) -o $@ $^ $(LIBS)
@@ -21,8 +22,9 @@ $(TARGET): $(OBJECTS) ymlib/libymlib.a
 	$(CC) $(CFLAGS) -o $<
 
 clean:
-	-rm -f $(TARGET) $(OBJECTS)
+	-rm -f $(TARGET) $(OBJECTS) minislug.opk
 	-make -C ymlib -f Makefile_a clean
+.PHONY: clean
 
 ymlib/libymlib.a:
 	make CC=$(CC) CXX=$(CXX) -C ymlib -f Makefile_a
